fbbf8f24aa515d6543a91c9910a745d338e67b74
TonyPythoneer <tonypythoneer@gmail.com>
2017-05-08 15:46:56 +0800

    [ENHANCE] Add cluster and clear up web application server

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,29 @@
 import * as express from 'express';
-import LineBot = require('linebot');
-const packageJson = require('./../package.json');
-import * as api from './google-spreadsheet-api';
-import * as _ from 'lodash';
+import { bot } from './seria';
+import * as cluster from 'cluster';
+import { cpus } from 'os';
 
-const VERSION = packageJson.version;
 
-const { CHANNEL_ID, CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN } = process.env;
+const numCPUs = cpus().length;
 
-let bot = LineBot({
-    channelId: CHANNEL_ID,
-    channelSecret: CHANNEL_SECRET,
-    channelAccessToken: CHANNEL_ACCESS_TOKEN
-});
 
-interface Event {
-    type: string;
-    replyToken: string;
-    source: {
-        userId: string;
-        type: string;
-        profile: Function[];
-    };
-    timestamp: Number;
-    message: {
-        type: string;
-        id: string;
-        text: string;
-        content: Function[];
-    };
-    reply: (kind: string) => Promise<string>;
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    for (let i = 0; i < numCPUs; i++) { cluster.fork(); }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
+    const linebotParser = bot.parser();
+    app.post('/', linebotParser);
+
+    const server = app.listen(process.env.PORT || 8080, function () {
+        let port = server.address().port;
+        console.log(`* Application is running on port ${port}'`);
+    });
+
+    console.log(`Worker ${process.pid} started`);
 }
-
-
-bot.on('join', (event: Event) => {
-    event.reply('你好啊！冒險者');
-});
-
-
-bot.on('leave', (event: Event) => {
-    event.reply('再見了！冒險者');
-});
-
-
-function turtleCheckerHandler(command: string[]) {
-    switch (command[0]) {
-        case 'uncheck':
-            break;
-        default:
-            break;
-    }
-}
-
-
-async function turtleHandler(event: Event, command: string[]) {
-    switch (command[0]) {
-        case 'sun':
-        case 'sat':
-        case 'wed':
-            let weekday = _.capitalize(command[0]);
-            let content = await api.readTable(weekday);
-            await event.reply(content);
-            break;
-        default:
-            break;
-    }
-}
-
-
-bot.on('message', (event: Event) => {
-    if (event.message.type !== 'text') return;
-    let command = event.message.text.split(' ');
-    console.log(event.message.text);
-    console.log(command);
-    let length = command.length;
-    if (length < 1) return;
-    if (command[0] === 'seria') command = command.splice(1, length);
-
-    switch (command[0]) {
-        case 'help':
-            event.reply('此功能開發中！');
-            break;
-        case 'turtle':
-            turtleHandler(event, command.splice(1, command.length));
-            break;
-        case 'version':
-            event.reply(`目前版本為 ${VERSION} 唷！`);
-            break;
-        default:
-            break;
-    }
-});
-
-
-/**
- * Application server
- */
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-const server = app.listen(process.env.PORT || 8080, function () {
-    let port = server.address().port;
-    console.log('App now running on port', port);
-});
---
+++ src/seria.ts
@@ -0,0 +1,93 @@
+import LineBot = require('linebot');
+const packageJson = require('./../package.json');
+import * as api from './google-spreadsheet-api';
+import * as _ from 'lodash';
+
+
+const VERSION = packageJson.version;
+
+const { CHANNEL_ID, CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN } = process.env;
+
+export let bot = LineBot({
+    channelId: CHANNEL_ID,
+    channelSecret: CHANNEL_SECRET,
+    channelAccessToken: CHANNEL_ACCESS_TOKEN
+});
+
+interface Event {
+    type: string;
+    replyToken: string;
+    source: {
+        userId: string;
+        type: string;
+        profile: Function[];
+    };
+    timestamp: Number;
+    message: {
+        type: string;
+        id: string;
+        text: string;
+        content: Function[];
+    };
+    reply: (kind: string) => Promise<string>;
+}
+
+
+bot.on('join', (event: Event) => {
+    event.reply('你好啊！冒險者');
+});
+
+
+bot.on('leave', (event: Event) => {
+    event.reply('再見了！冒險者');
+});
+
+
+function turtleCheckerHandler(command: string[]) {
+    switch (command[0]) {
+        case 'uncheck':
+            break;
+        default:
+            break;
+    }
+}
+
+
+async function turtleHandler(event: Event, command: string[]) {
+    switch (command[0]) {
+        case 'sun':
+        case 'sat':
+        case 'wed':
+            let weekday = _.capitalize(command[0]);
+            let content = await api.readTable(weekday);
+            await event.reply(content);
+            break;
+        default:
+            break;
+    }
+}
+
+
+bot.on('message', (event: Event) => {
+    if (event.message.type !== 'text') return;
+    let command = event.message.text.split(' ');
+    console.log(event.message.text);
+    console.log(command);
+    let length = command.length;
+    if (length < 1) return;
+    if (command[0] === 'seria') command = command.splice(1, length);
+
+    switch (command[0]) {
+        case 'help':
+            event.reply('此功能開發中！');
+            break;
+        case 'turtle':
+            turtleHandler(event, command.splice(1, command.length));
+            break;
+        case 'version':
+            event.reply(`目前版本為 ${VERSION} 唷！`);
+            break;
+        default:
+            break;
+    }
+});
