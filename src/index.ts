b79f74ba3dec52782907f8a1fb4698cad97f2dc2
TonyPythoneer <tonypythoneer@gmail.com>
2017-05-08 16:01:31 +0800

    [ENHANCE] Improve the imports and log message

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import * as express from 'express';
-import { bot } from './seria';
 import * as cluster from 'cluster';
+import * as express from 'express';
 import { cpus } from 'os';
 
+import { bot } from './seria';
 
-const numCPUs = cpus().length;
+
+const CPU_NUM = cpus().length;
+const PORT = process.env.PORT || 8080;
 
 
 if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
+    console.log(`Web application server is running on port ${PORT} with ${CPU_NUM} CPUs`);
 
-    for (let i = 0; i < numCPUs; i++) { cluster.fork(); }
+    for (let i = 0; i < CPU_NUM; i++) { cluster.fork(); }
 
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
     });
 } else {
     const app = express();
+
     const linebotParser = bot.parser();
     app.post('/', linebotParser);
 
-    const server = app.listen(process.env.PORT || 8080, function () {
-        let port = server.address().port;
-        console.log(`* Application is running on port ${port}'`);
-    });
+    const server = app.listen(PORT);
 
     console.log(`Worker ${process.pid} started`);
 }
